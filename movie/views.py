8068ab1496c6b1fe21caf7b38b4d5cccea10302d
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-08-02 20:15:45 -0500

    Trabajo Completo

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,4 @@
+from turtle import color
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,3 +8,5 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Juan Esteban Pavas González'})
+def about(request):
+    return render(request, 'about.html')
