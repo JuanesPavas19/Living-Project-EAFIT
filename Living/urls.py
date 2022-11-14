80c1a55c375b8b867649aa97374caab4b58323b4
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-11-13 21:15:27 -0500

    Sin Cambios

--- Living/urls.py
+++ Living/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from movie import views as movieViews
+#from movie import views as movieViews
 from django.urls import include
 
 urlpatterns = [
