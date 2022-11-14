c5f856c297070628b934dacb49f9bbd8f2088374
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-11-13 21:17:34 -0500

    Cambios para poder revisar usuario y Contacto

--- movie/admin.py
+++ movie/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Dieta, Rutina
+from .models import Dieta, Rutina, Usuario, Contacto, Imc
 # Register your models here.
 admin.site.register(Dieta)
 admin.site.register(Rutina)
+admin.site.register(Usuario)
+admin.site.register(Contacto)
+admin.site.register(Imc)
