3134383a276897ea0a15be7e7d4eb2acad57fd4e
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-11-13 21:24:01 -0500

    Se crearon las vistas de contacto resgitro, etc y se hace el enlace con los modelos y forms.py

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,8 @@
-from ctypes.wintypes import HENHMETAFILE
-from turtle import color
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dieta, Rutina
+from .forms import UsuarioForm, ContactoForm, ImcForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,28 +18,83 @@ def home(request):
     #return render(request, 'home.html', {'name': 'Juan Esteban Pavas González'})
     #return HttpResponse("<h1>Bienevnido a Living</h1>")
     return render (request, 'home.html')
+
 def about(request):
     return render(request, 'about.html')
+
 def dietas(request):
-    return render(request, 'dietas.html')
+    dietas = Dieta.objects.all()
+    return render(request, 'dietas.html', {'dietas': dietas })
+
 def rutinas(request):
-    return render(request, 'rutinas.html')
+    rutinas = Rutina.objects.all()
+    return render(request, 'rutinas.html', {'rutinas': rutinas})
 
 #dietas:
 def d1(request):
     return render(request, 'dietas/d1.html')
+
 def d2(request):
     return render(request, 'dietas/d2.html')
+
 def d3(request):
     return render(request, 'dietas/d3.html')
 #rutinas:
 def r1(request):
     return render(request, 'rutinas/r1.html')
+
 def r2(request):
     return render(request, 'rutinas/r2.html')
+
 def r3(request):
     return render(request, 'rutinas/r3.html')
 
 #formulario:
 def form(request):
-    return render(request, 'formulario/form.html')
+    formulario = ImcForm(request.POST or None)
+    ##imc = ImcForm(request=imcs)
+    #if imc < 18.5:
+    #    messages.success(request, "Rango de Peso Insuficiente")
+    #    return redirect('form')
+    #elif imc >= 18.5 or imc < 25.0 :
+    #    messages.success(request, "Rango de Peso Normal o Saludable")
+     #   return redirect('form')
+    #elif imc >= 25.0 or imc < 30.0 :
+    #    messages.success(request, "Rango de Sobrepeso")
+    #    return redirect('form')
+    #elif imc > 29.9:
+    #    messages.success(request, "Rango de Obesidad")
+    #    return redirect('form')  
+    return render(request, 'formulario/form.html', {'formulario': formulario})#, {'imc': imc})
+
+def register(request):
+    formulario = UsuarioForm(request.POST or None)
+    if formulario.is_valid():
+        formulario.save()
+        messages.success(request, "Datos Registrados","tus Datos fueron registrados con exito", "error")  # prueba de funcionalidad
+        return redirect('home')
+    return render(request, 'formulario/register.html', {'formulario': formulario})
+
+def login(request):
+    return render(request, 'formulario/login.html')
+
+def contacto(request):
+    data = {
+        "form": ContactoForm()
+    }
+    if request.method == 'POST':
+        formulario = ContactoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "contacto guardado"
+        else:
+            data["form"] = formulario
+    return render(request, 'formulario/contacto.html', data)
+
+def contacto1(request):
+    formulario = ContactoForm(request.POST or None)
+    if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Comentario Enviado")
+            return redirect('contacto1')
+    return render(request, 'formulario/contacto1.html', {'formulario': formulario})
