ac61706efb4887b846a5e0621e2f07064314f2ca
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-11-15 08:07:48 -0500

    Pruebas

--- Living/settings.py
+++ Living/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6-&sms%0%xtbsprt+6to%-l4m^(mgao0ekt8u5l$gdb&l#v1l7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.134.217.109']
+ALLOWED_HOSTS = ['34.121.38.109']
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
